8d1303d60e5d99edca753ea8cfd45e4afdc74d52
Attila Gasparetz <45799912+GatiMedia@users.noreply.github.com>
2020-01-05 22:11:15 +0000

    Create rep.py

---
+++ rep.py
@@ -0,0 +1,48 @@
+import nuke
+
+thisNode = nuke.thisGroup()
+
+Copies1 = thisNode[copy1].value()
+
+Copies2 = thisNode[copy2].value()
+
+cDot = nuke.nodes.Dot()
+
+cFirstLoop = True
+
+for i in range(Copies1):
+    cTrans = nuke.nodes.Transform(name = "t" + str(i), translate = 20 20)
+    cMerge = nuke.nodes.Merge2(name = "m" + str(i))
+    cMerge.setInput(1,cTrans)
+
+    if cFirstLoop:
+        cFirstLoop = False
+        cTrans.setInput(0, cDot)
+        cMerge.setInput(0, cDot)
+    else:
+        cTrans.setInput(0, cprevMerge)
+        cMerge.setInput(0, cprevMerge)
+
+    cprevMerge = cMerge
+
+#####
+
+iRepeats = 10
+bfirstLoop = True
+
+nDot = nuke.nodes.Dot()
+
+for i in range(iRepeats):
+    nTrans = nuke.nodes.Transform(name = "t" + str(i), translate = "20 20")
+    nMerge = nuke.nodes.Merge2(name = "m" + str(i))
+    nMerge.setInput(1, nTrans)
+
+    if bfirstLoop:
+        bfirstLoop = False
+        nTrans.setInput(0, nDot)
+        nMerge.setInput(0, nDot)
+    else:
+        nTrans.setInput(0, nPrevMerge)
+        nMerge.setInput(0, nPrevMerge)
+
+    nPrevMerge = nMerge
