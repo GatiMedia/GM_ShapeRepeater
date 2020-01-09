1870966b75fab70470a421663d603e9d56e015e9
Attila Gasparetz <45799912+GatiMedia@users.noreply.github.com>
2020-01-09 23:48:49 +0000

    Update rep.py

--- rep.py
+++ rep.py
@@ -1,40 +1,29 @@
+# --------------------------------------------------------------
+#  shape_repeater.py
+#  Version: 1.0.0
+#  Author: Attila Gasparetz
+#
+#  Last Modified by: Attila Gasparetz
+#  Last Updated:  05/01/2020
+# --------------------------------------------------------------
+
+#####
+
 import nuke
 
 thisNode = nuke.thisGroup()
 
-Copies1 = thisNode[copy1].value()
+iRepeats1 = thisNode.knob("copy1").getValue()
 
-Copies2 = thisNode[copy2].value()
+iRepeats = int(iRepeats1)
 
-cDot = nuke.nodes.Dot()
-
-cFirstLoop = True
-
-for i in range(Copies1):
-    cTrans = nuke.nodes.Transform(name = "t" + str(i), translate = 20 20)
-    cMerge = nuke.nodes.Merge2(name = "m" + str(i))
-    cMerge.setInput(1,cTrans)
-
-    if cFirstLoop:
-        cFirstLoop = False
-        cTrans.setInput(0, cDot)
-        cMerge.setInput(0, cDot)
-    else:
-        cTrans.setInput(0, cprevMerge)
-        cMerge.setInput(0, cprevMerge)
-
-    cprevMerge = cMerge
-
-#####
-
-iRepeats = 10
 bfirstLoop = True
 
 nDot = nuke.nodes.Dot()
 
 for i in range(iRepeats):
-    nTrans = nuke.nodes.Transform(name = "t" + str(i), translate = "20 20")
-    nMerge = nuke.nodes.Merge2(name = "m" + str(i))
+    nTrans = nuke.nodes.Transform(name="t" + str(i), translate="20 20")
+    nMerge = nuke.nodes.Merge2(name="m" + str(i))
     nMerge.setInput(1, nTrans)
 
     if bfirstLoop:
@@ -46,38 +35,141 @@ for i in range(iRepeats):
         nMerge.setInput(0, nPrevMerge)
 
     nPrevMerge = nMerge
-    
-#####
 
+#####
 
 import nuke
 
-thisNode = nuke.thisGroup()
+thisNode = nuke.thisNode()
 
-thisNode.begin()
-    
-copy1 = nuke.toNode('PROXY_MAIN').knob('copy1').getvalue()
+cCopies_1 = thisNode.knob("copy1").getValue()
+cCopies_2 = thisNode.knob("copy2").getValue()
 
-copy2 = nuke.toNode('PROXY_MAIN').knob('copy2').getvalue()
+cCopies1 = int(cCopies_1)
+cCopies2 = int(cCopies_2)
 
-cDot = nuke.nodes.Dot()
+CfirstLoop1 = True
+CfirstLoop2 = True
 
-cFirstLoop = True
+CDot = nuke.nodes.Dot()
 
-for i in range(copy1):
-    cTrans = nuke.nodes.Transform(name = "t" + str(i))
-    cMerge = nuke.nodes.Merge2(name = "m" + str(i))
-    cMerge.setInput(1,cTrans)
+CopyTrans1 = nuke.toNode('Trans_COPY1')
+CopyTrans2 = nuke.toNode('Trans_COPY2')
 
-    if cFirstLoop:
-        cFirstLoop = False
-        cTrans.setInput(0, cDot)
-        cMerge.setInput(0, cDot)
-    else:
-        cTrans.setInput(0, cprevMerge)
-        cMerge.setInput(0, cprevMerge)
+ for i in range(CfirstLoop1):
+     cTrans = nuke.nodes.Transform(name="t" + str(i))
+     cMerge = nuke.nodes.Merge2(name="m" + str(i))
+     cMerge.setInput(1,cTrans)
 
-    cprevMerge = cMerge
+     if cfirstLoop1:
+         cfirstLoop = False
+         cTrans.setInput(0, CopyTrans1)
+         cMerge.setInput(0, CopyTrans1)
+     else:
+         cTrans.setInput(0, cPrevMerge)
+         cMerge.setInput(0, cPrevMerge)
+
+     cPrevMerge = cMerge
+
+#####
+
+CopyTrans1 = nuke.toNode('Trans_COPY1')
+CopyTrans2 = nuke.toNode('Trans_COPY2')
+
+m = nuke.thisNode()
+kc = nuke.thisKnob()
+if kc.name() in ["copy1"]:
+    i = nuke.toNode('Trans_COPY1')
+    o = nuke.toNode('EndCopy1')
+
+    slices = int(['copy1'].value())
+    step = int(1)
+    for x in range(1,slices+step,step):
+
+        b1 = nuke.nodes.Blur()
+        b1.setInput(0, i)
+        b1['size'].setExpression("parent.size.w", 0)
+        b1['size'].setExpression("parent.size.h", 1)
+
+        m1 = nuke.nodes.Merge()
+        m1.setInput(1, b1)
+        m1.setInput(0, g1)
+        m1['operation'].setValue("divide")
+
+        if x < slices:
+            m2 = nuke.nodes.Merge()
+        m2.setInput(1, b2)
+        m2.setInput(0, g1)
+        m2['operation'].setValue("divide")
+
+        i = m1
+        i2 = m2
+
+        o.setInput(0, i)
+
+####
+
+thisNode = nuke.thisNode()
+
+cCopies_1 = thisNode.knob("copy1").getValue()
+cCopies_2 = thisNode.knob("copy2").getValue()
+
+cCopies1 = int(cCopies_1)
+cCopies2 = int(cCopies_2)
+
+CopyTrans1 = nuke.toNode('Trans_COPY1')
+CopyTrans2 = nuke.toNode('Trans_COPY2')
+
+CTrans = nuke.nodes.Transform(name="t" + str(i))
+CTrans.knob('translate').setExpression('Trans_COPY1.translate')
+CTrans.knob('rotate').setExpression('Trans_COPY1.rotate')
+CTrans.knob('scale').setExpression('Trans_COPY1.scale')
+CTrans.knob('skewX').setExpression('Trans_COPY1.skewX')
+CTrans.knob('skewY').setExpression('Trans_COPY1.skewY')
+CTrans.knob('skew_order').setExpression('Trans_COPY1.skew_order')
+CTrans.knob('center').setExpression('Trans_COPY1.center')
+CTrans.knob('invert_matrix').setExpression('Trans_COPY1.invert_matrix')
+CTrans.knob('filter').setExpression('Trans_COPY1.filter')
+
+CTrans.setInput(0,CopyTrans1)
+
+CMerge = nuke.nodes.Merge2(name="m" + str(i))
+
+CMerge.setInput(0, CTrans)
+
+###
+
+w = nuke.toNode('Transform25')
+v = nuke.createNode('Transform')
+p = nuke.createNode('Merge2')
+v.setInput(0, v)
+p.setInput(0, w)
+
+####
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
