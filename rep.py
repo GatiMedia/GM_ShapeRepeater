b23c748f4fad023801e754e6ae9028fd59291602
Attila Gasparetz <45799912+GatiMedia@users.noreply.github.com>
2020-01-18 19:36:09 +0000

    Update rep.py

--- rep.py
+++ rep.py
@@ -306,6 +306,77 @@ if kc.name() in ["copy1"]:
 
     o.setInput(0, i)')
 
+#################
+                                                           
+                                                           
+                                                           
+
+Rep = nuke.toNode('GM_ShapeRepeater3')
+
+Rep.begin()
+
+nuke.toNode('PROXY_MAIN1').knob('knobChanged').setValue("""
+m = nuke.thisNode()
+kc = nuke.thisKnob()
+
+if kc.name() in ["copy1"]:
+
+    for n in nuke.allNodes():
+        if "static" not in n['label'].getValue():
+            nuke.delete(n)
+
+    
+    iRep = m.knob('copy1').getValue()
+    iRepeats = int(iRep)
+    bfirstLoop = True
+    
+    # Main Transform for Copy1
+    w = nuke.toNode('Trans_COPY1')
+    
+    # Last Merge connected to this
+    b = nuke.toNode('COPIES1_end')
+    
+    # Dot would be connected to this and allows toggle between original and modified source 
+    s = nuke.toNode('Switch1')
+    
+    nDot = nuke.nodes.Dot()
+    nDot.setInput(0, s)
+    
+    for i in range(iRepeats):
+        CTrans = nuke.nodes.Transform(name = "t" + str(i))
+        CTrans.knob('translate').setExpression('Trans_COPY1.translate')
+        CTrans.knob('rotate').setExpression('Trans_COPY1.rotate')
+        CTrans.knob('scale').setExpression('Trans_COPY1.scale')
+        CTrans.knob('skewX').setExpression('Trans_COPY1.skewX')
+        CTrans.knob('skewY').setExpression('Trans_COPY1.skewY')
+        CTrans.knob('skew_order').setExpression('Trans_COPY1.skew_order')
+        CTrans.knob('center').setExpression('Trans_COPY1.center')
+        CTrans.knob('invert_matrix').setExpression('Trans_COPY1.invert_matrix')
+        CTrans.knob('filter').setExpression('Trans_COPY1.filter')
+        nMerge = nuke.nodes.Merge2(name = "m" + str(i))
+        nMerge.knob('also_merge').setValue('all')
+        nMerge.setInput(1, CTrans)
+        
+        if bfirstLoop:
+            bfirstLoop = False
+            CTrans.setInput(0, nDot)
+            nMerge.setInput(0, nDot)
+        else:
+            CTrans.setInput(0, nPrevMerge)
+            nMerge.setInput(0, nPrevMerge)
+    
+        nPrevMerge = nMerge
+    
+    MNum = int(iRepeats) - 1
+    
+    p = nuke.toNode("m" + str(MNum))
+    
+    b.setInput(0, p)
+""")
+
+
+Rep.end()
+
 
 
 
